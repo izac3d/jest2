ba5a11e882d487002b89538dc217ed6c68a11152
Isaac <izac3d@gmail.com>
2022-09-04 17:31:14 +0200

    Initial commit

---
+++ mytest.test.js
@@ -0,0 +1,12 @@
+let a = 2;
+let b = 3;
+
+it("a + b should be 5", () => {
+  expect(a + b).toBe(5);
+  expect(a-b).toBe(-1);
+});
+
+it("a - b should be -1", () => {
+  
+  expect(a-b).toBe(-1);
+});
